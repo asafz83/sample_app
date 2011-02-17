cbf29012b3a05de80702b7dac9faab90b4b5a65d
Asaf <asafz83@gmail.com>
2011-02-17 17:59:56 +0200

    Done with sign-in-out

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,12 @@ class User < ActiveRecord::Base
 		return nil if user.nil?
 		return user if user.has_password?(submitted_password)
 		#if we got here - the password mismatched - and nill will be returned as default
-	end
+  end
+
+  def self.authenticate_with_salt(id, cookie_salt)
+    user = find_by_id(id)
+    (user && user.salt == cookie_salt) ? user : nil
+  end
 
 private
 
